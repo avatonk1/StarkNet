50cf127fd17f4a54a220073bd214bcae7c26a312
avatonk1 <89200617+avatonk1@users.noreply.github.com>
2022-07-05 19:04:11 +0300

    Create Supdate.sh

---
+++ Supdate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "DBS DAO приветствует Вас."
+echo "Запуск обновления... "
+cd ~/pathfinder/py
+git fetch &>/dev/null
+git checkout v0.2.4-alpha &>/dev/null
+echo "Репозиторий успешно обновлен, начинаем билд"
+echo "-----------------------------------------------------------------------------"
+python3 -m venv .venv &>/dev/null
+source .venv/bin/activate &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt &>/dev/null
+cargo build --release --bin pathfinder &>/dev/null
+sleep 2
+source $HOME/.bash_profile &>/dev/null
+echo "Билд завершен успешно"
+echo "-----------------------------------------------------------------------------"
+systemctl restart starknet
+echo "Нода обновлена"
